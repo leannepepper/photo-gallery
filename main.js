96420e3ce093517ba2cedfbcfe2bbee377cf9833
LeannePepper <leanne.werner.lw@gmail.com>
2019-02-10 12:59:40 -0800

    added responsive layout and cleaned up js

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
+const searchInput = document.querySelector("#search");
+
 function searchFunction() {
-  // Declare variables
-  var input = document.querySelector("#search");
-  var filter = input.value.toUpperCase(); 
-  var galleryList = document.querySelector(".gallery-container");
-  var galleryItem = galleryList.querySelectorAll(".img-container");
+
+  const input = document.querySelector("#search");
+  const filter = input.value.toUpperCase(); 
+  const galleryList = document.querySelector(".gallery-container");
+  const galleryItem = galleryList.querySelectorAll(".img-container");
   
     for(let i = 0; i < galleryItem.length; i++){
-      var captions = galleryItem[i].getAttribute("data-title"); 
-        // console.log(captions.toUpperCase());
-        if(captions.toUpperCase().search(filter) > -1) {
-          galleryItem[i].style.display = ""; 
-        } else {
-          galleryItem[i].style.display = "none";
-        }
+      const captions = galleryItem[i].getAttribute("data-title") 
+      const isFiltered = captions.toUpperCase().search(filter) > -1
+      galleryItem[i].style.display = isFiltered ? "" : "none"; 
     }
   }
+  searchInput.addEventListener('keydown', searchFunction); 
